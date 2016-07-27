7a65c825debdfe74c13312d4913e4313ae0f2ca0
helgabykova <mookkerenok@gmail.com>
2016-07-27 10:44:20 +0300

    Add files via upload

---
+++ Calculator.feature
@@ -0,0 +1,35 @@
+﻿Feature: Calculator  
+In order to check the functionality
+As a user
+I should find Sum, Dev, Multi and Minus of numbers 
+
+	
+@mytag
+Scenario: Plus two numbers
+	Given I have entered 5 into the calculator
+	And I entered 5 into the calculator 
+	When I have entered +
+	Then the Sum result should be 10 on the screen
+
+
+Scenario: Minus two numbers
+	Given I have entered 30 into the calculator
+	And I entered 5 into the calculator
+	When I have entered -
+	Then the Minus result should be 25 on the screen
+
+
+Scenario: Multi two numbers
+	Given I have entered 5 into the calculator
+	And I entered 5 into the calculator
+	When I have entered *
+	Then the Multi result should be 25 on the screen
+
+	
+Scenario:  Div two numbers
+	Given I have entered 40 into the calculator
+	And I entered 5 into the calculator
+	When I have entered /
+	Then the Div result should be 8 on the screen
+
+
---
+++ CalculatorSteps.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Calculator;
+using NUnit.Framework;
+
+namespace Calculator
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        ReaderAndOutput r;
+        double result;
+        double a;
+        double b;
+
+        //First number entering
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(string number)
+        {
+            a = Convert.ToDouble(number);
+        }
+
+        //Second number entering
+        [Given(@"I entered (.*) into the calculator")]
+        public void WhenIEnteredIntoTheCalculator(string number)
+        {
+            b = Convert.ToDouble(number);
+        }
+
+        //Plus function
+        [When(@"I have entered \+")]
+        public void WhenIHaveEntered()
+        {
+            r = new ReaderAndOutput(a, b);
+            result = MathOperations.Sum(r);
+        }
+    
+        [Then(@"the Sum result should be (.*) on the screen")]
+        public void ThenTheSumResultShouldBeOnTheScreen(string getresult)
+        {
+            Assert.AreEqual(result, Convert.ToDouble(getresult));
+        }
+
+        //Multi function
+        [When(@"I have entered \*")]
+        public void GivenIHaveEnteredMultiSign()
+        {
+            r = new ReaderAndOutput(a, b);
+            result = MathOperations.Multi(r);
+        }
+
+        [Then(@"the Multi result should be (.*) on the screen")]
+        public void ThenTheMultiResultShouldBeOnTheScreen(string getresult)
+        {
+            Assert.AreEqual(result, Convert.ToDouble(getresult));
+        }
+
+        //Div function
+        [When(@"I have entered \/")]
+        public void GivenIHaveEnteredDivSign()
+        {
+            r = new ReaderAndOutput(a, b);
+            result = MathOperations.Div(r);
+        }
+
+        [Then(@"the Div result should be (.*) on the screen")]
+        public void ThenTheDivResultShouldBeOnTheScreen(string getresult)
+        {
+            Assert.AreEqual(result, Convert.ToDouble(getresult));
+        }
+
+        //Minus function
+        [When(@"I have entered \-")]
+        public void GivenIHaveEnteredMinusSign()
+        {
+            r = new ReaderAndOutput(a, b);
+            result = MathOperations.Minus(r);
+        }
+        [Then(@"the Minus result should be (.*) on the screen")]
+        public void ThenTheMinusResultShouldBeOnTheScreen(string getresult)
+        {
+            Assert.AreEqual(result, Convert.ToDouble(getresult));
+        }
+
+
+    }
+}
